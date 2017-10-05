2f79cf3217ea35156f8ce4e60e51d643fe40457c
Ben Chuba <benjamin.chuba@gmail.com>
2017-10-05 22:01:25 +0100

    more norman

--- keyboards/ergodox_ez/keymaps/norman/keymap.c
+++ keyboards/ergodox_ez/keymaps/norman/keymap.c
@@ -29,25 +29,25 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *   | LK5  | LA1  |  LK2 |  LA3 |  LA4 |                                       |   [  |   ] |   /  |   \  |   @   |
  *   `----------------------------------'                                       `----------------------------------'
  *                                        ,-------------.       ,-------------.
- *                                        | App  | LGui |       | Alt  |Ctrl/Esc|
+ *                                        | Left | Right|       | Home |  End |
  *                                 ,------|------|------|       |------+--------+------.
- *                                 |      |      | Home |       | PgUp |        |      |
- *                                 | Space|Backsp|------|       |------|  Tab   |Enter |
- *                                 |      |ace   | End  |       | PgDn |        |      |
+ *                                 |      |      |  Up  |       | PgUp |        |      |
+ *                                 |Backsp|  Del |------|       |------| Enter  |Space |
+ *                                 |ace   |      | Down |       | PgDn |        |      |
  *                                 `--------------------'       `----------------------'
  */
 // If it accepts an argument (i.e, is a function), it doesn't need KC_.
 // Otherwise, it needs KC_*
 [BASE] = LAYOUT_ergodox(  // layer 0 : default
         // left hand
-        KC_EQL,         KC_1,         KC_2,   KC_3,   KC_4,   KC_5,   KC_LEFT,
-        KC_DELT,        KC_Q,         KC_W,   KC_E,   KC_R,   KC_T,   TG(SYMB),
-        KC_BSPC,        KC_A,         KC_S,   KC_D,   KC_F,   KC_G,
-        KC_LSFT,        CTL_T(KC_Z),  KC_X,   KC_C,   KC_V,   KC_B,   ALL_T(KC_NO),
-        LT(SYMB,KC_GRV),KC_QUOT,      LALT(KC_LSFT),  KC_LEFT,KC_RGHT,
-                                              ALT_T(KC_APP),  KC_LGUI,
-                                                              KC_HOME,
-                                               KC_SPC,KC_BSPC,KC_END,
+        KC_ESC,         KC_1  ,         KC_2 ,   KC_3 ,   KC_4 ,   KC_5,   KC_GRV ,
+        KC_TAB,         KC_Q  ,         KC_W ,   KC_D ,   KC_F ,   KC_K,   KC_LGUI,
+        KC_LSFT,        KC_A  ,         KC_S ,   KC_E ,   KC_T ,   KC_G,
+        KC_LCTL,        KC_Z  ,         KC_X ,   KC_C ,   KC_V ,   KC_B,   KC_LALT,
+        TG(5)  ,        OSL(1),         TG(2),   OSL(3),  OSL(4),
+																				   KC_LEFT,KC_RIGHT,
+																							  KC_UP,
+																		   KC_BSPC,KC_DEL,  KC_DOWN,
         // right hand
              KC_RGHT,     KC_6,   KC_7,  KC_8,   KC_9,   KC_0,             KC_MINS,
              TG(SYMB),    KC_Y,   KC_U,  KC_I,   KC_O,   KC_P,             KC_BSLS,
