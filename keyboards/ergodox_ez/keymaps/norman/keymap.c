c2847ff7ee45bb6ae0b65ae85cc9e3c8bd73a2e5
Ben Chuba <benjamin.chuba@gmail.com>
2017-10-06 15:31:07 +0100

    migrate the synergy keys over to macros so I can debounce alt and ctrl after switch

--- keyboards/ergodox_ez/keymaps/norman/keymap.c
+++ keyboards/ergodox_ez/keymaps/norman/keymap.c
@@ -13,8 +13,6 @@
 #define THMUP LCTL(LSFT(KC_U))
 #define THMDWN LCTL(LSFT(KC_D))
 #define AUDSRC LCTL(LSFT(KC_GRV))
-#define SDESKT LCTL(KC_GRV)
-#define SLAPTO LCTL(LALT(KC_GRV))
 #define DIM LCTL(LSFT(KC_0))
 
 enum custom_keycodes {
@@ -25,6 +23,8 @@ enum custom_keycodes {
   SHRUG1,
   EYES,
   WHAT1,
+  SLAPTO,
+  SDESKT,
   DYNAMIC_MACRO_RANGE
 };
 
@@ -368,6 +368,18 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 		if (record->event.pressed) {
 			SEND_STRING("What the fuck did you just fucking say about me, you little bitch? I'll have you know I graduated top of my class in the Navy Seals, and I've been involved in numerous secret raids on Al-Quaeda, and I have over 300 confirmed kills. I am trained in gorilla warfare and I'm the top sniper in the entire US armed forces. You are nothing to me but just another target. I will wipe you the fuck out with precision the likes of which has never been seen before on this Earth, mark my fucking words. You think you can get away with saying that shit to me over the Internet? Think again, fucker. As we speak I am contacting my secret network of spies across the USA and your IP is being traced right now so you better prepare for the storm, maggot. The storm that wipes out the pathetic little thing you call your life. You're fucking dead, kid. I can be anywhere, anytime, and I can kill you in over seven hundred ways, and that's just with my bare hands. Not only am I extensively trained in unarmed combat, but I have access to the entire arsenal of the United States Marine Corps and I will use it to its full extent to wipe your miserable ass off the face of the continent, you little shit. If only you could have known what unholy retribution your little 'clever' comment was about to bring down upon you, maybe you would have held your fucking tongue. But you couldn't, you didn't, and now you're paying the price, you goddamn idiot. I will shit fury all over you and you will drown in it. You're fucking dead, kiddo.");
 		}
+	case SDESKT:
+		if (record->event.pressed) {
+			SEND_STRING(SS_DOWN(X_LCTRL)SS_TAP(X_GRAVE)SS_UP(X_LCTRL));
+			SEND_STRING(SS_DOWN(X_LCTRL)SS_UP(X_LCTRL));
+		}
+	break;
+	case SLAPTO:
+		if (record->event.pressed) {
+			SEND_STRING(SS_DOWN(X_LCTRL)SS_DOWN(X_LALT)SS_TAP(X_GRAVE)SS_UP(X_LCTRL)SS_UP(X_LALT));
+			SEND_STRING(SS_DOWN(X_LCTRL)SS_DOWN(X_LALT)SS_UP(X_LCTRL)SS_UP(X_LALT));
+		}
+	break;
   }
   return true;
 }
@@ -376,7 +388,6 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 void matrix_init_user(void) {
 
 };
-
 
 // Runs constantly in the background, in a loop.
 void matrix_scan_user(void) {
