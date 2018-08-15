1e94f5993ce98206af7f998793c5b5839b20c4e9
Ben Chuba <benjamin.chuba@gmail.com>
2018-08-15 08:28:00 -0400

    quick swap

--- keyboards/katana60/keymaps/norman/keymap.c
+++ keyboards/katana60/keymaps/norman/keymap.c
@@ -227,14 +227,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,  KC_Q,    KC_W,    KC_D,    KC_F,    KC_K,    KC_LBRC,          KC_RBRC, KC_J,    KC_U,    KC_R,    KC_L,    KC_SCLN, KC_BSLS,
   KC_LSFT, KC_A,    KC_S,    KC_E,    KC_T,    KC_G,    KC_HOME,          KC_PGUP, KC_Y,    KC_N,    KC_I,    KC_O,    KC_H,    KC_ENT,
   KC_LCTL, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_END,  KC_DEL,  KC_PGDN, KC_P,    KC_M,    KC_COMM, KC_DOT,  KC_UP,   KC_QUES,
-  KC_PERC, KC_LGUI, KC_LALT, LT(2, KC_SLSH),            KC_BSPC, KPIR,    KC_SPC,           KC_F5,   KC_QUOT, KC_LEFT, KC_DOWN, KC_RIGHT
+  KC_PERC, KC_LGUI, KC_LALT, LT(2, KC_SLSH),            KC_BSPC, KPIR,    KC_SPC,           KC_QUOT, KC_F5,   KC_LEFT, KC_DOWN, KC_RIGHT
     ),
  [QWER] = LAYOUT( /* Base */
   KC_ESC,  KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    TO(0),   KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_PLUS,
   KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_LBRC,          KC_RBRC, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,
   KC_LSFT, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_HOME,          KC_PGUP, KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT,
   KC_LCTL, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_END,  KC_DEL,  KC_PGDN, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_UP,   KC_QUES,
-  KC_PERC, KC_LGUI, KC_LALT, LT(2, KC_SLSH),            KC_BSPC, KPIR,    KC_SPC,           KC_F5,   KC_QUOT, KC_LEFT, KC_DOWN, KC_RIGHT
+  KC_PERC, KC_LGUI, KC_LALT, LT(2, KC_SLSH),            KC_BSPC, KPIR,    KC_SPC,           KC_QUOT, KC_F5,   KC_LEFT, KC_DOWN, KC_RIGHT
     ),
 
 [FUNT] = LAYOUT(
@@ -242,7 +242,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   SFTR,    KC_BTN1, KC_MS_U, KC_BTN2, KC_WH_U, WINT,    KC_F11,           KC_F12,  RETB,    WINE,    EYES,    BEAR,    LENNY,   KC_PSCR,
   KC_LSFT, KC_MS_L, KC_MS_D, KC_MS_R, KC_WH_D, AVLD,    KC_VOLD,          KC_VOLU, AVLU,    WAOT,    MINI,    CLAP,    THNK,    KC_ENT,
   KC_LCTL, AXIN,    LKTW,    CN75,    CNMS,    AVLM,    KC_MPRV, KC_MPLY, KC_MNXT, KC_MUTE, TSMN,    CALL,    CRYL,    THUP,    TADA,
-  DIM,     KC_LGUI, KC_LALT, KC_TRNS,                   LPTP,    SHRG,    DESK,             NTPD,    TG(3),   OKAY,    THDN,    FIRE
+  DIM,     KC_LGUI, KC_LALT, KC_TRNS,                   LPTP,    SHRG,    DESK,             TG(3),   NTPD,   OKAY,    THDN,    FIRE
     ),
 [SWAP] = LAYOUT(
   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
